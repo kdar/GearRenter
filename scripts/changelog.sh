d631760c25ee46e04c34af5d7aba01a4f63542fd
Kevin Darlington <kevin@outroot.com>
2015-01-20 15:09:49 -0500

    removed scripts directory

--- scripts/changelog.sh
+++
@@ -1,4 +0,0 @@
-TAG1=$(git describe --abbrev=0 --tags `git rev-list --tags --max-count=2` | head -n 1)
-TAG2=$(git describe --abbrev=0 --tags `git rev-list --tags --max-count=2` | tail -n 1)
-
-git log --no-merges --format="* %h %s" ${TAG2}...${TAG1} > CHANGELOG.txt
